ddb06b1da97da616adeac93e8d1b88cfbc7d95cd
Arian Zanjani <aria0127@edu.zealand.dk>
2022-11-09 11:34:32 +0100

    cockstring

---
+++ DrRest/Managers/DrContext.cs
@@ -0,0 +1,10 @@
+﻿using DrRest.Models;
+using Microsoft.EntityFrameworkCore;
+namespace DrRest.Managers
+{
+    public class DrContext : DbContext
+    {
+        public DrContext(DbContextOptions<DrContext> options) : base(options) { }
+        public DbSet<Music> music { get; set; }
+    }
+}
